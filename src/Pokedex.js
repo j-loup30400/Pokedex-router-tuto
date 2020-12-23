09ddcd9af60792ee93d1957925f5ac4e712d5a98
Cayuela Jean-Loup <jeanloupcayuela30400@gmail.com>
2020-12-23 15:23:34 +0000

    First commit

---
+++ src/Pokedex.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Pokedex = () => {
+    return(
+        <div>
+            this is the pokedex page
+        </div>
+    )
+}
+
+export default Pokedex;
---
+++ src/Pokemon.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Pokemon = () => {
+    return(
+        <div>
+            this is the pokemon page
+        </div>
+    )
+}
+
+export default Pokemon;
